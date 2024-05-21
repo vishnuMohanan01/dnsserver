edb62a4a2efbd4663158b803c40456f605e2be88
vishnuMohonan01 <iamvm.dev@gmail.com>
2024-05-22 01:27:00 +0530

    Adds methods to read 2 and 4 bytes from current position

--- src/main.rs
+++ src/main.rs
@@ -4,62 +4,95 @@ pub struct PacketBuffer {
 }
 
 impl PacketBuffer {
-    pub fn new() -> PacketBuffer {
-        PacketBuffer {
-            buf: [0; 512],
-            pos: 0
-        }
+  pub fn new() -> PacketBuffer {
+    PacketBuffer {
+      buf: [0; 512],
+      pos: 0
+    }
+  }
+
+  pub fn pos(&self) -> usize {
+    self.pos
+  }
+
+  pub fn step(&mut self, steps: usize) -> Result<(), ()> {
+    self.pos += steps;
+
+    Ok(())
+  }
+
+  pub fn seek(&mut self, pos: usize) -> Result<(), ()> {
+    self.pos = pos;
+
+    Ok(())
+  }
+
+  pub fn read(&mut self) -> Result<u8, &str> {
+    if self.pos >= 512 {
+      return Err("End of Buffer");
     }
 
-    pub fn pos(&self) -> usize {
-        self.pos
+    let result = self.buf[self.pos];
+    self.pos += 1;
+
+    Ok(result)
+  }
+
+  pub fn get(&self, pos: usize) -> Result<u8, &str> {
+    if pos >= 512 {
+      return Err("Position out of bound");
     }
 
-    pub fn step(&mut self, steps: usize) -> Result<(), ()> {
-        self.pos += steps;
+    Ok(self.buf[self.pos])
+  }
 
-        Ok(())
+  pub fn get_range(&self, start: usize, len: usize) -> Result<&[u8], &str> {
+    if start + len >= 512 {
+      return Err("Position out of bound");
     }
 
-    pub fn seek(&mut self, pos: usize) -> Result<(), ()> {
-        self.pos = pos;
+    Ok(&self.buf[start..start + len as usize])
+  }
 
-        Ok(())
+  pub fn read_u16(&mut self) -> Result<u16, &str> {
+    if self.pos >= 512 {
+      return Err("Position out of bounds");
     }
+    
+    let mut result: u16 = 0;
+    
+    result = result | (self.buf[self.pos] as u16) << 8;
+    self.pos += 1;
+    
+    result = result | self.buf[self.pos] as u16;
+    self.pos += 1;
 
-    pub fn read(&mut self) -> Result<u8, &str> {
-        if self.pos >= 512 {
-            return Err("End of Buffer");
-        }
-
-        let result = self.buf[self.pos];
-        self.pos += 1;
-
-        Ok(result)
+    Ok(result)
+  }
+  
+  pub fn read_u32(&mut self) -> Result<u32, &str> {
+    if self.pos >= 512 {
+      return Err("Position out of bounds");
     }
+    
+    let mut result: u32 = 0;
+    
+    result = result | (self.buf[self.pos] as u32) << 24;
+    self.pos += 1;
+    
+    result = result | (self.buf[self.pos] as u32) << 16;
+    self.pos += 1;
+    
+    result = result | (self.buf[self.pos] as u32) << 8;
+    self.pos += 1;
+    
+    result = result | self.buf[self.pos] as u32;
+    self.pos += 1;
+    
+    Ok(result)
+  }
+}
 
-    pub fn get(&self, pos: usize) -> Result<u8, &str> {
-        if pos >= 512 {
-            return Err("Position out of bound");
-        }
-
-        Ok(self.buf[self.pos])
-    }
-
-    pub fn get_range(&self, start: usize, len: usize) -> Result<&[u8], &str> {
-        if start + len >= 512 {
-            return Err("Position out of bound");
-        }
-
-        Ok(&self.buf[start..start + len as usize])
-    }
-
-    pub fn read_u16(&mut self) -> Result<u16, &str> {
-        // FIXME: We are calling read() twice which uses &mut self
-        let ms_byte = self.read()? as u16;
-        let ls_byte = self.read()? as u16;
-        let result = (ms_byte << 8) | ls_byte;
-
-        Ok(result)
-    }
+pub fn main() {
+  println!("hello");
 }
